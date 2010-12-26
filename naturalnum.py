21532df3fc26b04e54cb1b0820b664af77b536c9
Ben Rowland <ben.rowland@gmail.com>
2010-12-26 18:24:10 +0000

    moved rule logic into its own object

--- naturalnum.py
+++ naturalnum.py
@@ -4,116 +4,146 @@ lhsValidatorRegex = r"[0-9a-zA-Z]+"
 rhsValidatorRegex = r"[0-9a-zA-Z\(\),\$]+"  ## Only alphanumerics or these chars: (),$
 ruleValidatorRegex = r".+=.+"               ## At least one char each side of '=' delimiter
 
-def validateAndParseRule(rule):
-	"""Validates and parses entire rule.  Any characters after '#' are ignored.
-		If any characters remain, the remainder must have a '=' delimiter, surrounded by
-		a valid LHS and RHS (which will be validated by the corresponding functions).
-
-		Return values:
-		- If entire rule is valid, a tuple containing LHS and RHS will be returned
-		- If rule is invalid, None will be returned
-		- If rule was only a comment, an empty tuple will be returned
+class Rule:
+	"""Holds all attributes of a Rule, 
+		including matching and translation logic.
 	"""
 
-	# Get rule without comments, return () if rule is only a comment
+	def __init__(self, lhs=None, rhs=None):
+		self.lhs = lhs
+		self.rhs = rhs
+
+	def init(self):
+		self.validateLhs()
+		self.validateRhs()
+		self.validateLhsWithRhs()
+		self.initd = True
+
+	def validateLhs(self):
+		"""Validates string in LHS of the rule.  All of the following must be true:
+         - not empty or null
+         - alphanumeric chars only i.e. [0-9a-Z]
+     	   - if alpha chars are present they must all be unique, as they will be
+   	     used as identifiers
+		"""	
+		# Ensure lhs is present
+		if (self.lhs == None or self.lhs == ""):
+			raise RuleValidationException("Could not validate lhs of Rule: [" + 
+				"" if self.lhs is None else self.lhs + "] because it is empty.")
+
+		# Ensure lhs is alphanumeric only
+		if (re.match(lhsValidatorRegex, self.lhs) == None):
+			raise RuleValidationException("Could not validate lhs of Rule: [" + 
+				self.lhs + "].  Only alpha/digits allowed.")
+	
+		# If alpha chars present, ensure they are unique
+		if(not self.lhs.isdigit()):
+			alphas = [x for x in self.lhs if not x.isdigit()]
+			if len(set(alphas)) != len(alphas):
+				raise RuleValidationException("Could not validate lhs of Rule: [" + 
+					self.lhs + "].  Alpha characters must be unique.")
+
+	def validateRhs(self):
+		"""Validates string in RHS of a rule.  All of the following must be true:
+			- Not empty or null
+			- Only alphanumeric chars, or any of: (),$
+			- Brackets must be balanced
+		"""
+		# Check rhs is present
+		if (self.rhs == None or self.rhs == ""):
+			raise RuleValidationException("Could not validate rhs of Rule: [" + 
+				"" if self.rhs is None else self.rhs + "] because it is empty.")
+	
+		# Check only allowed chars present
+		if (re.match(rhsValidatorRegex, self.rhs) == None):
+			raise RuleValidationException("Could not validate rhs of Rule: [" + 
+				self.rhs + "].  Only alpha/digits or the following chars allowed: (),$")
+
+		# Check brackets are balanced
+		depth = 0
+		brackets = [x for x in self.rhs if x in ('(', ')')]
+		for x in brackets:
+			if x == '(':
+				depth = depth + 1
+			else:
+				depth = depth - 1
+		if depth != 0:
+			raise RuleValidationException("Could not validate rhs of Rule: [" + 
+				self.rhs + "] - brackets are unbalanced.")
+	
+		# Check '$' only precedes an alpha character
+		checkNextIsAlpha = False
+		for x in self.rhs:
+			if x == '$':	
+				checkNextIsAlpha = True
+			else:
+				if checkNextIsAlpha:
+					if not x.isalpha():
+						raise RuleValidationException("Could not validate rhs of Rule: [" + 
+							self.rhs + "].  '$' must always be folowed by an alpha char.")
+					checkNextIsAlpha = False
+
+	def validateLhsWithRhs(self):
+		# Check all placeholders on RHS appear on LHS
+		# 1. Find set of all LHS placeholders
+		lhsPlaceholders = set([x for x in self.lhs if not x.isdigit()])
+
+		# 2. Find set of all RHS placeholders
+		placeholderDetected = False
+		rhsPlaceholdersList = []	
+		for x in self.rhs:
+			if x == '$':
+				placeholderDetected = True
+			else:
+				if placeholderDetected:
+					rhsPlaceholdersList.append(x)
+					placeholderDetected = False
+		rhsPlaceholders = set(rhsPlaceholdersList)
+	
+		# 3. Check RHS placeholders is a subset of LHS placeholders 
+		# (not necessarily vice-versa)
+		if not rhsPlaceholders <= lhsPlaceholders:
+			raise RuleValidationException("Could not validate Rule lhs [" + 
+				self.lhs + "] with rhs: [" + self.rhs + 
+				"] - placeholder(s) on rhs do not appear on lhs.")
+
+class RuleValidationException(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
+def validateAndParseRule(rule):
+	"""Validates and parses entire rule string, returning it as a Rule object.  
+		Any characters after '#' in the rule string are ignored.
+		The rule string must have a '=' delimiter, surrounded by a valid LHS and 
+		RHS.
+
+		Return values:
+		- If entire rule is valid, a Rule object
+		- If rule is invalid, a RuleValidationException will be raised
+		- If rule was only a comment, None will be returned
+	"""
+
+	# Get rule without comments, return None if rule is only a comment
 	commentCharPos = rule.find('#')
 	if commentCharPos != -1:
 		ruleWithoutComment = rule[:commentCharPos].rstrip()
 	else:
 		ruleWithoutComment = rule.rstrip()
-
 	if len(ruleWithoutComment) == 0:
-		return ()
+		return None
 	
 	# Check that rule without comment is some chars, delimited by '='
 	if (re.match(ruleValidatorRegex, ruleWithoutComment) == None):
-		return None	
+		raise RuleValidationException("Could not validate rule: [" + 
+			rule + "].  Format should be: <lhs>=<rhs>")
 
-	# Split rule into LHS and RHS and validate those separately
+	# Split rule into LHS and RHS, create Rule object and run its validations
 	parts = ruleWithoutComment.split('=')
 	lhs = parts[0]
 	rhs = parts[1]
-	if validateLhs(lhs):
-		if validateRhs(rhs):
-			return (lhs, rhs)
-		else:	
-			return None
-	else:
-		return None
+	rule = Rule(lhs, rhs)
+	rule.init()
+	return rule
 
-def validateLhs(lhs):
-	"""Validates string in LHS of a rule.  All of the following must be true:
-        - not empty or null
-        - alphanumeric chars only i.e. [0-9a-Z]
-        - if alpha chars are present they must all be unique, as they will be
-          used as identifiers
-	"""	
-	if (lhs == None or lhs == ""):
-		return False
-
-	if (re.match(lhsValidatorRegex, lhs) == None):
-		return False
-	
-	if(lhs.isdigit()): return True
-	else:
-		alphas = [x for x in lhs if not x.isdigit()]
-		return len(set(alphas)) == len(alphas)
-
-def validateRhs(rhs):
-	"""Validates string in RHS of a rule.  All of the following must be true:
-		- Not empty or null
-		- Only alphanumeric chars, or any of: (),$
-		- Brackets must be balanced
-	"""
-	# Check rhs is present
-	if (rhs == None or rhs == ""):
-		return False
-
-	# Check only allowed chars present
-	if (re.match(rhsValidatorRegex, rhs) == None):
-		return False
-
-	# Check brackets are balanced
-	depth = 0
-	brackets = [x for x in rhs if x in ('(', ')')]
-	for x in brackets:
-		if x == '(':
-			depth = depth + 1
-		else:
-			depth = depth - 1
-	if depth != 0:
-		return False
-
-	# Check '$' only precedes an alpha character
-	checkNextIsAlpha = False
-	for x in rhs:
-		if x == '$':	
-			checkNextIsAlpha = True
-		else:
-			if checkNextIsAlpha:
-				if not x.isalpha():
-					return False
-				checkNextIsAlpha = False 
-	return True
-
-def validateLhsWithRhs(lhs, rhs):
-	# Check all placeholders on RHS appear on LHS
-	# 1. Find set of all LHS placeholders
-	lhsPlaceholders = set([x for x in lhs if not x.isdigit()])
-
-	# 2. Find set of all RHS placeholders
-	placeholderDetected = False
-	rhsPlaceholdersList = []	
-	for x in rhs:
-		if x == '$':
-			placeholderDetected = True
-		else:
-			if placeholderDetected:
-				rhsPlaceholdersList.append(x)
-				placeholderDetected = False
-	rhsPlaceholders = set(rhsPlaceholdersList)
-	
-	# 3. Check RHS placeholders is a subset of LHS placeholders (not necessarily vice-versa)
-	if not rhsPlaceholders <= lhsPlaceholders:
-		return False
-	return True
--- naturalnum_tests.py
+++ naturalnum_tests.py
@@ -1,58 +1,79 @@
 import unittest
-import naturalnum
+from naturalnum import *
 
 class TestNaturalNum(unittest.TestCase):
 	#def setUp(self):
 		
 	def testValidateLhs(self):
 		## Ensure only alphanumerics allowed
-		self.assertEqual(False, naturalnum.validateLhs(""))
-		self.assertEqual(False, naturalnum.validateLhs(None))
-		self.assertEqual(False, naturalnum.validateLhs("."))
-		self.assertEqual(True, naturalnum.validateLhs("1"))
-		self.assertEqual(True, naturalnum.validateLhs("123"))
-		self.assertEqual(True, naturalnum.validateLhs("h"))
-		self.assertEqual(True, naturalnum.validateLhs("htu"))
+		self.assertRaises(RuleValidationException, Rule("", None).validateLhs)
+		self.assertRaises(RuleValidationException, Rule(None, None).validateLhs)
+		self.assertRaises(RuleValidationException, Rule(".", None).validateLhs)
+		self.assertRaises(RuleValidationException, Rule("", None).validateLhs)
+		try:
+			Rule("1", None).validateLhs()
+			Rule("123", None).validateLhs()
+			Rule("h", None).validateLhs()
+			Rule("htu", None).validateLhs()
+		except RuleValidationException:
+			self.fail("No validation exception should be raised")
 
 		## Ensure no repetition of alpha chars allowed
-		self.assertEqual(False, naturalnum.validateLhs("hh"))
-		self.assertEqual(False, naturalnum.validateLhs("h1h"))
+		self.assertRaises(RuleValidationException, Rule("hh", None).validateLhs)
+		self.assertRaises(RuleValidationException, Rule("h1h", None).validateLhs)
 
 	def testValidateRhs(self):
 		## Ensure only alphanumerics, '(', ')', ',', '$' allowed
-		self.assertEqual(False, naturalnum.validateRhs(""))
-		self.assertEqual(False, naturalnum.validateRhs(None))
-		self.assertEqual(False, naturalnum.validateRhs("."))
-		self.assertEqual(False, naturalnum.validateRhs(" "))
-		self.assertEqual(True, naturalnum.validateRhs("Aa1(),$"))	
+		self.assertRaises(RuleValidationException, Rule(None, "").validateRhs)
+		self.assertRaises(RuleValidationException, Rule(None, None).validateRhs)
+		self.assertRaises(RuleValidationException, Rule(None, ".").validateRhs)
+		self.assertRaises(RuleValidationException, Rule(None, " ").validateRhs)
+		try:
+			Rule(None, "Aa1(),$").validateRhs()
+		except RuleValidationException:
+			self.fail("Should not raise RuleValidationException")
 
 		## Ensure only balanced brackets allowed
-		self.assertEqual(False, naturalnum.validateRhs("(("))
-		self.assertEqual(False, naturalnum.validateRhs("()("))
-		self.assertEqual(True, naturalnum.validateRhs("()()"))
+		self.assertRaises(RuleValidationException, Rule(None, "((").validateRhs)
+		self.assertRaises(RuleValidationException, Rule(None, "()(").validateRhs)
+		try:		
+			Rule(None, "()()").validateRhs()
+		except RuleValidationException:
+			self.fail("Should not raise RuleValidationException")
 
 		## Ensure '$' only used in correct context (followed by alpha character)
-		self.assertEqual(False, naturalnum.validateRhs("$1"))
-		self.assertEqual(False, naturalnum.validateRhs("$("))
-		self.assertEqual(True, naturalnum.validateRhs("$a"))
+		self.assertRaises(RuleValidationException, Rule(None, "$1").validateRhs)
+		self.assertRaises(RuleValidationException, Rule(None, "$(").validateRhs)
+		try:
+			Rule(None, "$a").validateRhs()
+		except RuleValidationException:
+			self.fail("Should not raise RuleValidationException")
 
 	def testValidateLhsWithRhs(self):
 		## Ensure all chars on RHS preceded by $ appear on LHS
-		self.assertEqual(False, naturalnum.validateLhsWithRhs("a", "$d"))
-		self.assertEqual(False, naturalnum.validateLhsWithRhs("abc", "$a$b$d"))
-		self.assertEqual(True, naturalnum.validateLhsWithRhs("abc", "$a$b$c"))
+		self.assertRaises(RuleValidationException, Rule("a", "$d").validateLhsWithRhs)
+		self.assertRaises(RuleValidationException, Rule("abc", "$a$b$d").validateLhsWithRhs)
+		try:
+			Rule("abc", "$a$b$c").validateLhsWithRhs()
+		except RuleValidationException:
+			self.fail("Should not raise RuleValidationException")
 
 	def testValidateAndParseRule(self):
 		## These tests should always have a valid LHS and RHS, we're not testing LHS/RHS
 		## validations here
 
 		## Ensure commented lines validate OK
-		self.assertEqual((), naturalnum.validateAndParseRule("#this is a comment"))
-		self.assertEqual(("abc", "123"), naturalnum.validateAndParseRule("abc=123 #this is a comment"))
+		self.assertEqual(None, validateAndParseRule("#this is a comment"))
+		rule = validateAndParseRule("abc=123 #this is a comment")
+		self.assertEqual("abc", rule.lhs)
+		self.assertEqual("123", rule.rhs)
 
 		## Ensure only 2 sets of characters, delimited by '=' is allowed
-		self.assertEqual(None, naturalnum.validateAndParseRule("abcabc"))
-		self.assertEqual(("123a", "$a"), naturalnum.validateAndParseRule("123a=$a"))
-	
+		self.assertRaises(RuleValidationException, validateAndParseRule, "abcabc")
+		self.assertRaises(RuleValidationException, validateAndParseRule, "abcabc #=comment")
+		rule = validateAndParseRule("123a=$a")
+		self.assertEqual("123a", rule.lhs)
+		self.assertEqual("$a", rule.rhs)
+
 if __name__ == '__main__':
 	unittest.main()
