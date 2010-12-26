d789f71dd0bdc9bfcdbf795c04f8d597443d53a4
Ben Rowland <ben.rowland@gmail.com>
2010-12-26 14:25:25 +0000

    Adding code for rhs rule validation

--- naturalnum.py
+++ naturalnum.py
@@ -1,6 +1,7 @@
 import re
 
 lhsValidatorRegex = r"[0-9a-zA-Z]+"
+rhsValidatorRegex = r"[0-9a-zA-Z\(\),\$]+"  ## Only alphanumerics or these chars: (),$
 
 def validateLhs(lhs):
 	"""Validates string in LHS of a rule.  All of the following must be true:
@@ -8,8 +9,7 @@ def validateLhs(lhs):
         - alphanumeric chars only i.e. [0-9a-Z]
         - if alpha chars are present they must all be unique, as they will be
           used as identifiers
-	"""
-	
+	"""	
 	if (lhs == None or lhs == ""):
 		return False
 
@@ -20,3 +20,40 @@ def validateLhs(lhs):
 	else:
 		alphas = [x for x in lhs if not x.isdigit()]
 		return len(set(alphas)) == len(alphas)
+
+def validateRhs(rhs):
+	"""Validates string in RHS of a rule.  All of the following must be true:
+		- Not empty or null
+		- Only alphanumeric chars, or any of: (),$
+		- Brackets must be balanced
+	"""
+	# Check rhs is present
+	if (rhs == None or rhs == ""):
+		return False
+
+	# Check only allowed chars present
+	if (re.match(rhsValidatorRegex, rhs) == None):
+		return False
+
+	# Check brackets are balanced
+	depth = 0
+	brackets = [x for x in rhs if x in ('(', ')')]
+	for x in brackets:
+		if x == '(':
+			depth = depth + 1
+		else:
+			depth = depth - 1
+	if depth != 0:
+		return False
+
+	# Check '$' only precedes an alpha character
+	checkNextIsAlpha = False
+	for x in rhs:
+		if x == '$':	
+			checkNextIsAlpha = True
+		else:
+			if checkNextIsAlpha:
+				if not x.isalpha():
+					return False
+				checkNextIsAlpha = False 
+	return True
--- naturalnum_tests.py
+++ naturalnum_tests.py
@@ -8,18 +8,35 @@ class TestNaturalNum(unittest.TestCase):
 		## Ensure only alphanumerics allowed
 		self.assertEqual(False, naturalnum.validateLhs(""))
 		self.assertEqual(False, naturalnum.validateLhs(None))
-
 		self.assertEqual(False, naturalnum.validateLhs("."))
-
 		self.assertEqual(True, naturalnum.validateLhs("1"))
 		self.assertEqual(True, naturalnum.validateLhs("123"))
-
 		self.assertEqual(True, naturalnum.validateLhs("h"))
 		self.assertEqual(True, naturalnum.validateLhs("htu"))
 
 		## Ensure no repetition of alpha chars allowed
 		self.assertEqual(False, naturalnum.validateLhs("hh"))
 		self.assertEqual(False, naturalnum.validateLhs("h1h"))
+
+	def testValidateRhs(self):
+		## Ensure only alphanumerics, '(', ')', ',', '$' allowed
+		self.assertEqual(False, naturalnum.validateRhs(""))
+		self.assertEqual(False, naturalnum.validateRhs(None))
+		self.assertEqual(False, naturalnum.validateRhs("."))
+		self.assertEqual(False, naturalnum.validateRhs(" "))
+		self.assertEqual(True, naturalnum.validateRhs("Aa1(),$"))	
+
+		## Ensure only balanced brackets allowed
+		self.assertEqual(False, naturalnum.validateRhs("(("))
+		self.assertEqual(False, naturalnum.validateRhs("()("))
+		self.assertEqual(True, naturalnum.validateRhs("()()"))
+
+		## Ensure '$' only used in correct context (followed by alpha character)
+		self.assertEqual(False, naturalnum.validateRhs("$1"))
+		self.assertEqual(False, naturalnum.validateRhs("$("))
+		self.assertEqual(True, naturalnum.validateRhs("$a"))
+
+	#def testValidateLhsAndRhs(self):
 	
 if __name__ == '__main__':
 	unittest.main()
