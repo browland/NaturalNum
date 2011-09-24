82fb7be28db67f432da3992902b2ded56ae71e45
Ben Rowland <ben.rowland@gmail.com>
2011-09-24 12:33:20 +0100

    Moving site to Google App Engine

---
+++ site/README.txt
@@ -0,0 +1,1 @@
+This directory contains files for the Google App Engine format.
--- site/nn-json.py
+++ site/nn-json.py
@@ -1,35 +1,46 @@
-from mod_python import apache
-from mod_python import util
-from naturalnum import *
+import cgi
 
-def index(req):
-   """ Services requests via apache.  Requests would usually be submitted
-       by the AJAX form handler."""
-   docRoot = req.document_root()
-   form = util.FieldStorage(req,keep_blank_values=1)
-   lang = form.get("lang", None)
-   value = form.get("number", None)
-   json = callNaturalNum(docRoot, lang, value)
-   
-   return json
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from naturalnum import *
 
 def callNaturalNum(docRoot, lang, value):
    """ Delegates look-up of natural language represention of digits entered,
        by calling NaturalNum. """
-	configFileName = docRoot + "/naturalnum/config/" + lang + ".lang"
-	eng = RuleEngine.fromLangFilename(configFileName)
-	
-	return tokensToJson(eng.resolve(value))
+   configFileName = docRoot + "/config/" + lang + ".lang"
+   eng = RuleEngine.fromLangFilename(configFileName)
+   return tokensToJson(eng.resolve(value))
 
 def tokensToJson(tokens):
    """ Converts the list of strings returned by NaturalNum to JSON."""
-	json = '{"tokens": ['
+   json = '{"tokens": ['
 
-	for token in tokens:
-		json = json + '"' + token + '", '
+   for token in tokens:
+      json = json + '"' + token + '", '
 
-	# Append trailing closing array bracket and closing outer curly bracket
-	# We strip off the last 2 chars of the json because it will contain an
-	# extra unnecessary trailing comma and space.
-	json = json[0:-2] + ']}'
-	return json
+   # Append trailing closing array bracket and closing outer curly bracket
+   # We strip off the last 2 chars of the json because it will contain an
+   # extra unnecessary trailing comma and space.
+   json = json[0:-2] + ']}'
+   return json
+
+class JsonHandler (webapp.RequestHandler):
+	def get(self):
+		lang = self.request.get('lang')
+		value = self.request.get('value')
+		docRoot = '.'
+		json = callNaturalNum(docRoot, lang, value)
+		self.response.out.write(json)
+
+application = webapp.WSGIApplication(
+	[('/nn-json.py', JsonHandler)],
+	debug=True)
+	
+def main():
+	run_wsgi_app(application)
+	
+if __name__ == "__main__":
+	main()
+
+
